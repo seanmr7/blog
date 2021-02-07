8e37d41e0a4f0cf5ad0dfe675185bf748cf537f6
Sean Riedl <smriedl@gmail.com>
2021-02-07 17:52:11 -0500

    Revise credentials for live version

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :projects
   resources :posts
   get 'resume', to: 'resumes#resume'
-  devise_for :admins, skip: [:registrations]
+  devise_for :admins
   root to: "posts#index"
 end
