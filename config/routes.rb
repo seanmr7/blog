581d005aa5d179d81e9a30246e9b2ec50750cc90
Sean Riedl <smriedl@gmail.com>
2021-02-07 20:11:26 -0500

    Add sign-in button for admin access

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :projects, except: [:index, :show]
   resources :posts
   get 'resume', to: 'resumes#resume'
-  devise_for :admins, skip: [:registrations, :passwords]
+  devise_for :admins, skip: [:registrations]
   root to: "posts#index"
 end
